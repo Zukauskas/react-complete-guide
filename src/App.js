a4732359813382a7207cad300c9a71711d37f472
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-14 18:38:03 +0200

    Add ExpenseItem component and style it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-function App() {
+import ExpenseItem from './components/ExpenseItem'
+
+function App () {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
