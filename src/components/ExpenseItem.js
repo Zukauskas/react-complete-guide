ace4b7093745e938348c697028db14236971d9e0
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-15 23:35:56 +0200

    MCreating Expenses and Card components + props.childer

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,17 @@
+import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 import './ExpenseItem.css'
 
 function ExpenseItem({ title, amount, date }) {
-
-  const month = date.toLocaleString('en-US', { month: 'long' })
-  const day = date.toLocaleString('en-US', { day: '2-digit' })
-  const year = date.getFullYear()
-
-  return (
-    <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
-      </div>
-    </div>
-  )
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
+            </div>
+        </Card>
+    )
 }
 
 export default ExpenseItem
