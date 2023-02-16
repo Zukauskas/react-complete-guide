72bf5f4c3f8c2f61de0df48402458ceab3c1e489
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-16 19:49:31 +0200

    learning about state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
+import { useState } from 'react'
 
 function ExpenseItem({ title, amount, date }) {
+    const [newTitle, setNewTitle] = useState(title)
+
+    const clickHandler = () => {
+        setNewTitle('Updated!')
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{newTitle}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
