35617b404af2f958eb8d01ae9ed788c525fe274f
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-17 21:47:33 +0200

    Add filter component and pass around state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import ExpenseDate from './ExpenseDate'
-import Card from '../UI/Card'
-import './ExpenseItem.css'
-import { useState } from 'react'
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 function ExpenseItem({ title, amount, date }) {
-    const [newTitle, setNewTitle] = useState(title)
-
-    const clickHandler = () => {
-        setNewTitle('Updated!')
-    }
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={date} />
             <div className="expense-item__description">
-                <h2>{newTitle}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
-    )
+    );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,33 @@
-import './Expenses.css'
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import {useState} from 'react';
 
 function Expenses({ expensesData }) {
+   
+    const [dateFilter, setDateFilter] = useState('2020')
+
+    const filterValueHandler = (filter) => {
+           setDateFilter(filter)
+             }
+
+
     return (
-        <Card className="expenses">
-            {expensesData.map((expense) => (
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
-        </Card>
-    )
+        
+            <Card className="expenses">
+              <ExpensesFilter selectedYear={dateFilter} onChangeFilter={filterValueHandler}/> 
+                {expensesData.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
+            </Card>
+       
+    );
 }
 
-export default Expenses
+export default Expenses;
