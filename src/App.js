a0dee31f144340756027033d50334a345341ae53
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-17 12:35:24 +0200

    Managing state in Expense form component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Expenses from './components/Expenses/Expenses'
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
     const expenses = [
@@ -26,14 +27,14 @@ function App() {
             amount: 450,
             date: new Date(2021, 5, 12),
         },
-    ]
+    ];
 
     return (
         <div>
-            <h2>Let's get started!</h2>
+            <NewExpense />
             <Expenses expensesData={expenses} />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
